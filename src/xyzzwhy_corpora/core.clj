d618b45e7edff3a0bdd8d609a947b36b941a6083
Akiva <akiva@akivamail.com>
2015-11-02 09:31:37 -0600

    Adds database initialization.

--- src/xyzzwhy_corpora/core.clj
+++ src/xyzzwhy_corpora/core.clj
@@ -1,5 +1,33 @@
-(ns xyzzwhy-corpora.core)
+(ns xyzzwhy-corpora.core
+  (:require [environ.core :refer [env]]
+            [monger.core :as mg]))
+
+(def ^:private corpus {})
+(defonce db (atom nil))
+
+(defn initialize-db-connection!
+  [& {:keys [uri]}]
+  (reset! db (:db (mg/connect-via-uri (or uri (env :corpora-database-uri))))))
+
+(defn- pluralize
+  [c]
+  (let [c' (name c)]
+    (if (.endsWith c' "s")
+      c'
+      (str c' "s"))))
+
+(defn- initialize-corpus-from-namespace
+  [c n]
+  (reduce #(conj %1 @(-> (ns-resolve n (symbol %2)) deref future))
+          c
+          (-> (ns-resolve n 'classes) deref)))
+
+(defn- get-class
+  [c]
+  (-> c pluralize keyword corpus))
 
 (defn -main
   [& args]
-  (println "Nothing yet!"))
+  (initialize-db-connection!)
+
+  )
