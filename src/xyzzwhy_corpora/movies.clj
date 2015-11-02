a5f3d9cb5938bb0b3a5e4abf2c25b6d97414e99d
Akiva <akiva@akivamail.com>
2015-11-02 17:06:26 -0600

    Adds movies class.

---
+++ src/xyzzwhy_corpora/movies.clj
@@ -0,0 +1,11 @@
+(ns xyzzwhy-corpora.movies)
+
+(def movies
+   [
+    {:text "Breakin' 2: Electric Boogaloo"}
+    {:text "Coming to America"}
+    {:text "Debbie Does Dallas"}
+    {:text "Dirty Harry"}
+    {:text "Enter the Dragon"}
+    {:text "The Kentucky Fried Movie"}
+    ])
