756630f6a089dca5622cf21996f475134e45f755
Akiva <akiva@akivamail.com>
2015-11-05 10:55:53 -0600

    Converts adjectives class to new data structure.

--- src/xyzzwhy_corpora/text.clj
+++ src/xyzzwhy_corpora/text.clj
@@ -1677,33 +1677,6 @@
     {:text "violently points at"}
     ]})
 
-(def adjectives
-  {:adjectives
-   [{:text "afraid"}
-    {:text "angry"}
-    {:text "aroused"}
-    {:text "bored"}
-    {:text "distracted"}
-    {:text "forlorn"}
-    {:text "fussy"}
-    {:text "happy"}
-    {:text "hopeful"}
-    {:text "hungry"}
-    {:text "isolated"}
-    {:text "nonplussed"}
-    {:text "relieved"}
-    {:text "sad"}
-    {:text "sleepy"}
-    {:text "stinky"}
-    {:text "thirsty"}
-    {:text "uncertain"}
-    {:text "worried"}
-    {:text "vexed"}
-    ]})
-
-
-
-
 
 
 (def classes
