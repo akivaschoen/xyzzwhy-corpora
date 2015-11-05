b71ccca48954d84c0f77ca115d59a06739c00a9e
Akiva <akiva@akivamail.com>
2015-11-05 10:57:45 -0600

    Converts actions class to new data structure.

---
+++ src/xyzzwhy_corpora/actions.clj
@@ -0,0 +1,16 @@
+(ns xyzzwhy-corpora.actions)
+
+(def actions
+  {:actions
+   [
+    {:text "attacks"}
+    {:text "examines"}
+    {:text "flirts with"}
+    {:text "ignores"}
+    {:text "imitates"}
+    {:text "pets"}
+    {:text "sneezes on"}
+    {:text "stands uncomfortably close to"}
+    {:text "tickles"}
+    {:text "violently points at"}
+    ]})
--- src/xyzzwhy_corpora/text.clj
+++ src/xyzzwhy_corpora/text.clj
@@ -1663,19 +1663,7 @@
      :gender :male}
     ]})
 
-(def actions
-  {:actions
-   [{:text "attacks"}
-    {:text "examines"}
-    {:text "flirts with"}
-    {:text "ignores"}
-    {:text "imitates"}
-    {:text "pets"}
-    {:text "sneezes on"}
-    {:text "stands uncomfortably close to"}
-    {:text "tickles"}
-    {:text "violently points at"}
-    ]})
+
 
 
 
